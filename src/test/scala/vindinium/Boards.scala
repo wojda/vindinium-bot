dcda490aaade4ded966539a28d165ed638f46da4
Daniel Wojda <wojdadan@gmail.com>
2016-06-19 18:10:06 +0100

    Moved board from scala code into separate text file in order to increase readability. Logic responsible for reading from file had to be  extended because we lost trailing spaces during reading from file.

--- src/test/scala/vindinium/Boards.scala
+++ src/test/scala/vindinium/Boards.scala
@@ -4,12 +4,26 @@ import play.api.libs.json.Json
 import vindinium.bot.Board
 import vindinium.client.VindiniumProtocol
 
+import scala.io.Source
+
 trait Boards extends VindiniumProtocol {
-  val boardJson = """
-                    |{
-                    |   "size":10,
-                    |   "tiles":"##      ####      ##      ########@4            ####            []        []    $-    ##    ##    $-$-    ##    ##    $-    []      @3[]            ####              ########      ##@1  @2####      ##"
-                    | }
-                    |""".stripMargin
+
+  private val boardSize = 10
+  private val tailSize = 2
+  private val emptyLine = " " * (boardSize*tailSize)
+  private def completeWithSpaces(line: String): String = (line+emptyLine).take(boardSize*tailSize)
+
+  private val rawBoard = Source.fromURL(getClass.getResource("/board"))
+    .getLines
+    .map(completeWithSpaces)
+    .mkString
+
+  private val boardJson = s"""
+    |{
+    |   "size": $boardSize,
+    |   "tiles":"$rawBoard"
+    |}
+    |""".stripMargin
+
   val board = Json.parse(boardJson).as[Board]
 }
