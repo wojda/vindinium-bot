e4db69e6c6ba04fc3d83eb52f1279adc9f87f448
Daniel Wojda <wojdadan@gmail.com>
2016-06-20 13:12:42 +0100

    Refactored board interpretaion logic - tail recursion was used instead of nasty loop.

--- src/main/scala/vindinium/bot/board/interpret/BoardInterpreter.scala
+++ src/main/scala/vindinium/bot/board/interpret/BoardInterpreter.scala
@@ -1,9 +1,10 @@
 package vindinium.bot.board.interpret
 
 import vindinium.bot.Move._
-import vindinium.bot.Tile.Air
+import vindinium.bot.Tile.{Air, Mine}
 import vindinium.bot.{Board, Pos, Tile}
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 trait BoardInterpreter {
@@ -16,7 +17,7 @@ trait BoardInterpreter {
   def findPath(start: Pos, seekingTile: Tile, p: Path => Boolean = (_) => true): Option[Path] = {
     val newExplored = mutable.Set[Pos]()
 
-    def expand(explored: Stream[Path]): Stream[Path] = {
+    def explore(explored: Stream[Path]): Stream[Path] = {
       explored.flatMap(path => pathsToAllNeighbours(path))
         .filter(path => !newExplored.contains(path._1))
         .filter(path => validPos(path._1, seekingTile))
@@ -26,17 +27,18 @@ trait BoardInterpreter {
         })
     }
 
-    var paths = Stream((start, List[Move]()))
-    1 to 20 foreach { _ =>
-      paths = expand(paths)
-      paths
-        .find(p) match {
-        case x@Some(path) => return Some((path._1, path._2.reverse))
-        case None => ()
+    @tailrec
+    def doIt(paths: Stream[(Pos, List[Move])], predicate: Path => Boolean, counter: Int = 0): Option[Path] =
+      if(counter > 20) { None }
+      else {
+        val newPaths = explore(paths)
+        newPaths.find(predicate) match {
+          case x@Some(path) => Some((path._1, path._2.reverse))
+          case None => doIt(newPaths, predicate, counter+1)
+        }
       }
-    }
 
-    None
+    doIt(Stream((start, List[Move]())), p)
   }
 
   private def pathsToAllNeighbours(path: Path): Stream[Path] =
